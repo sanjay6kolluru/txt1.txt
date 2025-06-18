6d951a9c296c993e19a088fa921aa82908407c4a
sanjay6kolluru <sanjaykolluru6@outlook.com>
2025-06-18 12:12:34 +0530

    3.Assignment.py file added

---
+++ 3.Assignment.py
@@ -0,0 +1,8 @@
+#Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+#Input: nums = [1,2,3,4,5,6,7], k = 3
+#Output: [5,6,7,1,2,3,4]
+
+num=[1,2,3,4,5,6,7]
+k=3
+num[:]=num[-k:]+num[:-k]
+print(num)

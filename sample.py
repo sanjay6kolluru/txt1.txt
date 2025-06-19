afe8b9bc939a0e4184489f8985b340ba7c348017
sanjay6kolluru <sanjaykolluru6@outlook.com>
2025-06-19 20:59:34 +0530

    4.Assignment.py file added

--- sample.py
+++ sample.py
@@ -1,1 +1,10 @@
-print("Sai")
+from flask import Flask
+
+app = Flask(__name__)  # Create a Flask application
+
+@app.route('/')  # Define route for home page
+def home():
+    return "Hello, Flask!"  # Response shown on visiting "/"
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the server
